1c7e12f78d3bebc6d9caf8a2abdafa1168debc5c
headzoo <sean@headzoo.io>
2016-04-06 23:16:58 -0400

    Sidebar updated with now playing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var reddit = new Snoocore({
         secret: Config.reddit.oauth.secret,
         username: Config.reddit.oauth.username,
         password: Config.reddit.oauth.password,
-        scope: [ 'identity', 'read', 'vote', 'submit' ]
+        scope: [ 'identity', 'read', 'vote', 'edit', 'modconfig', 'submit' ]
     }
 });
 
@@ -20,6 +20,7 @@ bean.on('connect', function() {
         if (err) {
             throw err;
         }
+        
         reserveQueued();
         setInterval(reserveQueued, 60000);
     });
@@ -31,11 +32,22 @@ bean.connect();
 
 function buildComment(payload) {
     var text = payload.media.title + "\r\n\r\n";
-    text += "Queued By " + payload.queueby + "\r\n\r\n";
     if (payload.media.type == "yt") {
-        text += "https://www.youtube.com/watch?v=" + payload.media.id + "\r\n";
+        text += "https://www.youtube.com/watch?v=" + payload.media.id + "\r\n\r\n";
     }
+    text += "Queued By " + payload.queueby + "\r\n";
     
+    return text;
+}
+
+function buildSidebar(payload) {
+    var text = "---\r\n\r\n**Now Playing**  \r\n";
+    text += '*' + payload.media.title + "*  \r\n";
+    if (payload.media.type == "yt") {
+        text += "https://www.youtube.com/watch?v=" + payload.media.id + "  \r\n";
+    }
+    text += "Queued By " + payload.queueby + "\r\n\r\n---\r\n";
+
     return text;
 }
 
@@ -61,10 +73,27 @@ function reserveQueued() {
                             api_type: 'json',
                             text: buildComment(payload)
                         }).then(function () {
-                            bean.destroy(jobid, function (err) {
-                                if (err) {
-                                    console.log(err);
-                                }
+                        
+                            console.log('Updating sidebar');
+                            reddit('/r/pienudes/about/edit.json').get()
+                            .then(function(result) {
+                                var data = result.data;
+                                data.api_type = 'json';
+                                data.sr = data.subreddit_id;
+                                data.link_type = data.content_options;
+                                data.type = data.subreddit_type;
+                                
+                                var sidebar = buildSidebar(payload);
+                                sidebar += data.description.replace(/---([\s\S]*)\*\*Now Playing\*\*([\s\S]*)---/, '');
+                                data.description = sidebar;
+                                
+                                return reddit('/api/site_admin').post(data);
+                            }).then(function() {
+                                    bean.destroy(jobid, function (err) {
+                                        if (err) {
+                                            console.log(err);
+                                        }
+                                    });
                             });
                         });
                     }
